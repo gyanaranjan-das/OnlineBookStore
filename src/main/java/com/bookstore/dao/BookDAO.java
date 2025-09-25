3f7f0c26474f8321476ed3ace92ca99449c760cc
gyanaranjan-das <dasgyanaranjan835@gmail.com>
2025-09-25 09:55:28 +0530

    Created Register Servlet

--- src/main/java/com/bookstore/dao/BookDAO.java
+++ src/main/java/com/bookstore/dao/BookDAO.java
@@ -14,6 +14,52 @@ public class BookDAO {
 	}
 	
 	public List<Book> getAllBooks(){
-		List<Book> books
+		List<Book> books = new ArrayList<>();
+		String sql = "SELECT * FROM books";
+		
+		try {
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				Book book = new Book();
+				book.setId(rs.getInt("id"));
+				book.setTitle(rs.getString("title"));
+				book.setAuthor(rs.getString("author"));
+				book.setPrice(rs.getDouble("price"));
+				book.setImageUrl(rs.getString("image_url"));
+				book.setDescription(rs.getString("description"));
+				book.setStock(rs.getInt("stock"));
+				books.add(book);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return books;
 	}
-}
+		public Book getBookById(int id) {
+	        Book book = null;
+	        String sql = "SELECT * FROM books WHERE id = ?";
+
+	        try {
+	            PreparedStatement ps = connection.prepareStatement(sql);
+	            ps.setInt(1, id);
+	            ResultSet rs = ps.executeQuery();
+
+	            if (rs.next()) {
+	                book = new Book();
+	                book.setId(rs.getInt("id"));
+	                book.setTitle(rs.getString("title"));
+	                book.setAuthor(rs.getString("author"));
+	                book.setPrice(rs.getDouble("price"));
+	                book.setImageUrl(rs.getString("image_url"));
+	                book.setDescription(rs.getString("description"));
+	                book.setStock(rs.getInt("stock"));
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return book;
+	    }
+	}
+
