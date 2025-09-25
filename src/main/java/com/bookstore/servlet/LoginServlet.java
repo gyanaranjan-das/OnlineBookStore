11793ce4c47238fe5e30523b34abcb4682923ddb
gyanaranjan-das <dasgyanaranjan835@gmail.com>
2025-09-25 10:00:26 +0530

    created LoginServlet

--- src/main/java/com/bookstore/servlet/LoginServlet.java
+++ src/main/java/com/bookstore/servlet/LoginServlet.java
@@ -5,7 +5,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
+
+import com.bookstore.dao.UserDAO;
+import com.bookstore.model.User;
 
 /**
  * Servlet implementation class LoginServlet
@@ -14,28 +19,25 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+   protected void doPost(HttpServletResponse response,HttpsServletRequest request) 
+   	throws ServletException,IOException{
+	   
+	   //1. Get username and password from login form
+	   String username = request.getParameter("username");
+	   String password = request.getParameter("password");
+	   
+	   UserDAO userDAO = new UserDAO();
+	   User user = userDAO.loginUser(username, password);
+	   
+	   if(user!=null) {
+		   HttpSession session = request.getSession();
+		   
+		   session.setAttribute("loggedInUser",user);
+		   response.sendRedirect("index.jsp");
+	   }else {
+		   request.setAttribute("error Message","Invalid username or password.");
+		   request.getRequestDispatcher("login.jsp").forward(request,response);
+	   }
+   }
 
 }
